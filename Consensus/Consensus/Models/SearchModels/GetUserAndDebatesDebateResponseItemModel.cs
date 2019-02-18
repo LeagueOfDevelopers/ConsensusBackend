f2568aec02e051bc6daf7444f1f9eec6961f5fa9
yaroslav <bus.yaroslav@gmail.com>
2019-02-18 22:38:35 +0300

    app layer realization

---
+++ Consensus/Consensus/Models/SearchModels/GetUserAndDebatesDebateResponseItemModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Consensus.Models.SearchModels
+{
+    public class GetUserAndDebatesDebateResponseItemModel
+    {
+        /// <summary>
+        /// Название дебатов
+        /// </summary>
+        public string DebateTitle { get; }
+        /// <summary>
+        /// Id дебатов
+        /// </summary>
+        public Guid DebateIdentifier { get; }
+        /// <summary>
+        /// Статус дебатов
+        /// </summary>
+        public bool IsLive { get; }
+        /// <summary>
+        /// Категория дебатов
+        /// </summary>
+        public string Category { get; }
+        /// <summary>
+        /// Имя пригласившего
+        /// </summary>
+        public string InviterNickName { get; }
+        /// <summary>
+        /// Id пригласившего
+        /// </summary>
+        public Guid InviterIdentifier { get; }
+        /// <summary>
+        /// Имя приглашенного
+        /// </summary>
+        public string InvitedNickName { get; }
+        /// <summary>
+        /// Id приглашенного
+        /// </summary>
+        public Guid InvitedIdentifier { get; }
+
+        public GetUserAndDebatesDebateResponseItemModel(
+            string debateTitle,
+            Guid debateIdentifier,
+            bool isLive,
+            string category,
+            string inviterNickName,
+            Guid inviterIdentifier,
+            string invitedNickName,
+            Guid invitedIdentifier)
+        {
+            DebateTitle = debateTitle;
+            DebateIdentifier = debateIdentifier;
+            IsLive = isLive;
+            Category = category;
+            InviterNickName = inviterNickName;
+            InviterIdentifier = inviterIdentifier;
+            InvitedNickName = invitedNickName;
+            InvitedIdentifier = invitedIdentifier;
+        }
+    }
+}
---
+++ Consensus/Consensus/Models/SearchModels/GetUserAndDebatesUserResponseItemModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Consensus.Models.SearchModels
+{
+    public class GetUserAndDebatesUserResponseItemModel
+    {
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string NickName { get; }
+        /// <summary>
+        /// Id пользователя
+        /// </summary>
+        public Guid UserIdentifier { get; }
+        /// <summary>
+        /// Имя файла
+        /// </summary>
+        public string UserAvatarFileName { get; }
+        /// <summary>
+        /// Количество побед
+        /// </summary>
+        public int WinCount { get; }
+        /// <summary>
+        /// Количество поражений
+        /// </summary>
+        public int LossCount { get; }
+
+        public GetUserAndDebatesUserResponseItemModel(
+            string nickName,
+            Guid userIdentifier, 
+            string userAvatarFileName, 
+            int winCount, 
+            int lossCount)
+        {
+            NickName = nickName;
+            UserIdentifier = userIdentifier;
+            UserAvatarFileName = userAvatarFileName;
+            WinCount = winCount;
+            LossCount = lossCount;
+        }
+    }
+}
